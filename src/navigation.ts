d911552bb7196ec0083188e07f8c41b13ef8ce0d
Shrushti Sharma <sharmashrushti@icloud.com>
2026-08-29 04:42:47 +0530

    footer

--- src/navigation.ts
+++ src/navigation.ts
@@ -16,6 +16,6 @@ export const footerData = {
   secondaryLinks: [],
   socialLinks: [],
   footNote: `
-    © ${new Date().getFullYear()} Usha Consulting · All rights reserved.
+    © ${new Date().getFullYear()} Usha Consulting · All rights reserved. · Made by <a class="text-blue-600 hover:underline dark:text-blue-400" href="https://github.com/sharmashrushti" target="_blank" rel="noopener noreferrer">Shrushti Sharma</a>
   `,
-}; 
+};
